7b1d1a4acdbd30ca5b75c01246287525830f44b0
dmela <daniel.melander@luke.fi>
2022-06-28 15:41:53 +0300

    doc: descriptive docsting for thinning function

--- forestryfunctions/harvest/thinning.py
+++ forestryfunctions/harvest/thinning.py
@@ -7,6 +7,17 @@ def iterative_thinning(
         thin_predicate: Callable,
         extra_factor_solver: Callable
 ) -> Tuple[ForestStand, dict]:
+    """ Iteratively decreases the stem count of stand reference trees until stoppin condition is met.
+
+    The parameter :extra_factor_solver: may be used to customize the removal of stems.
+    If given as (lambda i,n,c: 0) removes same amount of stems from each tree (a.k.a even thinning).
+
+    :param stand: Forest stand instance of forestdatamodel library
+    :param thinning_factor: Intensity of the thinning on each iteration
+    :param thin_predicate: Condition to stop thinning
+    :param extra_factor_solver: Gradually increasing proportion of removal
+    :retuns: Thinned stand and number of removed stems
+    """
     n = len(stand.reference_trees)
     c = thinning_factor
     stems_removed = 0.0
