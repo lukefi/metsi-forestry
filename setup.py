a7500318321cafeece32b42cf5f62a545f82e583
dmela <daniel.melander@luke.fi>
2022-06-28 13:22:57 +0300

    chore: make to a package with setuptool

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="forestryfunctions",
+    description="Forestry functions for manipulating state of forest-data-model instances",
+    version="0.1.0",
+    packages=setuptools.find_namespace_packages(include=['forestryfunctions*']),
+)
