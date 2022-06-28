4a59f6f893491955d99f5eaef797c37c3495af04
dmela <daniel.melander@luke.fi>
2022-06-28 13:23:42 +0300

    chore: dependency to the forestdatamodel package

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     description="Forestry functions for manipulating state of forest-data-model instances",
     version="0.1.0",
     packages=setuptools.find_namespace_packages(include=['forestryfunctions*']),
+    dependency_links=['https://github.com/menu-hanke/forest-data-model@0.3.1#egg=forestdatamodel']
 )
