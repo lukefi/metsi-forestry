0c50c7a34e5553dc76e758149e148c2e7b5348b1
Niemelä Urho (LUKE) <urho.niemela@luke.fi>
2022-08-08 16:25:35 +0300

    feat: initialise thinning_output dict once and increment the trees' stems_removed_per_ha in each iteration

--- forestryfunctions/harvest/thinning.py
+++ forestryfunctions/harvest/thinning.py
@@ -23,33 +23,38 @@ def iterative_thinning(
     n = len(stand.reference_trees)
     c = thinning_factor
     # stems_removed = 0.0
-    thinning_output = {}
+
+    #initialises the thinning_output dictionary where stems_removed_per_ha is aggregated to during the thinning operation
+    thinning_output = {
+        rt.identifier: 
+            {
+                'stems_removed_per_ha': 0.0,
+                'species': rt.species,
+                'breast_height_diameter': rt.breast_height_diameter,
+                'height': rt.height,
+                'stems_per_ha': rt.stems_per_ha,
+                'stand_area': stand.area,
+            } 
+            for rt in stand.reference_trees
+        }
+
     while thin_predicate(stand):
         # cut until lower bound reached
         for i, rt in enumerate(stand.reference_trees):
             thin_factor = c + extra_factor_solver(i, n, c)
             thin_factor = 1.0 if thin_factor > 1.0 else thin_factor
             new_stems_per_ha = rt.stems_per_ha * thin_factor
-            # stems_removed += rt.stems_per_ha - new_stems_per_ha
-            stems_removed_per_ha = rt.stems_per_ha - new_stems_per_ha
+            thinning_output[rt.identifier]["stems_removed_per_ha"] += rt.stems_per_ha - new_stems_per_ha
             rt.stems_per_ha = new_stems_per_ha
 
-            # collect stems_removed and tree-level variables required by the cross cut function per reference tree to dict.
-            # this structure is currently stored in operation_aggregates separately for each thinning method and for each timing.
+            
             
             #NOTE: some considerations to address before merging
             # 1. Will collecting this data on per-reference_tree basis be a memory issue when running the sim with a full dataset?
             # 2. I'd also rather make thinning_output a class imported from the forest-data-model library.
             # 3. I'm currently assuming that since the new_aggregate is written per thinning_method and per time_point, 
             #  the thinning_output will not be written to twice (otherwise would need to accommodate it for updates)
-            thinning_output[rt.identifier] = {
-                'stems_removed_per_ha': stems_removed_per_ha,
-                'species': rt.species,
-                'breast_height_diameter': rt.breast_height_diameter,
-                'height': rt.height,
-                'stems_per_ha': rt.stems_per_ha,
-                'stand_area': stand.area,
-            }
-            
+    
+    # this structure is currently stored in operation_aggregates separately for each thinning method and for each timing.
     new_aggregate = {'thinning_output': thinning_output}
     return (stand, new_aggregate)
