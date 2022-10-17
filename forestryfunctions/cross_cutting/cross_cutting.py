63a396729045578ec373df9f9ea64614de7c8955
Niemelä Urho (LUKE) <urho.niemela@luke.fi>
2022-10-17 14:33:44 +0300

    chore: move non-api function to the top of the file

--- forestryfunctions/cross_cutting/cross_cutting.py
+++ forestryfunctions/cross_cutting/cross_cutting.py
@@ -1,11 +1,11 @@
 from typing import Any, Dict, List, Tuple
-from forestdatamodel.model import ForestStand
-from forestdatamodel.enums.internal import TreeSpecies
-from forestryfunctions.cross_cutting import stem_profile
-import numpy as np
-from forestryfunctions.cross_cutting.model import ThinningOutput
 
 import forestryfunctions.r_utils as r_utils
+import numpy as np
+from forestdatamodel.enums.internal import TreeSpecies
+from forestdatamodel.model import ForestStand
+from forestryfunctions.cross_cutting import stem_profile
+from forestryfunctions.cross_cutting.model import ThinningOutput
 
 _cross_cut_species_mapper = {
     TreeSpecies.PINE: "pine",
@@ -14,6 +14,66 @@ _cross_cut_species_mapper = {
     TreeSpecies.DOWNY_BIRCH: "birch",
     TreeSpecies.SILVER_BIRCH: "birch"
 }
+
+
+def apteeraus_Nasberg(T: np.ndarray, P: np.ndarray, m: int, n: int, div: int) -> tuple[np.ndarray, np.ndarray]:
+    """
+    This function has been ported from, and should be updated according to, the R implementation.
+    """
+    V = np.zeros(n)
+    C = np.zeros(n)
+    A = np.zeros(n)
+    L = np.zeros(n)
+
+    t = 1
+    d_top = 0.0
+    d_min = 0.0
+    v = 0.0
+    c = 0.0
+    v_tot = 0.0
+    c_tot = 0.0
+
+    for i in range(n): #iterate over div-length segmnents of the tree trunk
+        for j in range(m): #iterate over the number of timber assortment price classes (row count in puutavaralajimaarittelyt.txt)
+            # numpy array indexing: 1st row 2nd element: arr[0, 1]
+            # in R it's the same order: arr[2, 3] --> the item on 2nd row and 3rd column
+            # but whereas R-indexing is one-based, Python's is zero-based --> indexing has been offset by one
+            t = int(i + P[j,2] / div)
+            if t < n:
+                d_top = T[t,0]
+                d_min = P[j, 1]
+                
+                if d_top >= d_min:
+                    v = T[t,2] - T[i,2]
+                    c = v * P[j, 3]
+                    v_tot = v + V[i]
+                    c_tot = c + C[i]
+
+                    if c_tot > C[t]:
+                        V[t] = v_tot
+                        C[t] = c_tot
+                        A[t] = P[j, 0]
+                        L[t] = i
+                    
+    maxi = np.argmax(C)
+
+    nas = np.unique(P[:, 0])
+
+    volumes = np.zeros(len(nas))
+    values = np.zeros(len(nas))
+
+    a = l = 1
+
+    while maxi > 0:
+        a = int(A[maxi])-1 # a is used as an index for volumes and values. Here in Python, it must be subtracted by one to work with zero-based indexing.
+        l = int(L[maxi])
+        volumes[a] = volumes[a] + V[maxi] - V[l]
+        values[a] = values[a] + C[maxi] - C[l]
+
+        maxi = l
+
+    return (volumes, values)
+
 
 def _cross_cut(
         species: TreeSpecies,
@@ -124,68 +184,3 @@ def calculate_cross_cut_aggregates(volumes: List[List[float]], values: List[List
     total_value = sum(map(sum, values))
 
     return (total_volume, total_value)
-
-
-def apteeraus_Nasberg(T: np.ndarray, P: np.ndarray, m: int, n: int, div: int) -> tuple[np.ndarray, np.ndarray]:
-    """
-    This function has been ported from, and should be updated according to, the R implementation.
-    """
-    V = np.zeros(n)
-    C = np.zeros(n)
-    A = np.zeros(n)
-    L = np.zeros(n)
-
-    t = 1
-    d_top = 0.0
-    d_min = 0.0
-    v = 0.0
-    c = 0.0
-    v_tot = 0.0
-    c_tot = 0.0
-
-    for i in range(n): #iterate over div-length segmnents of the tree trunk
-        for j in range(m): #iterate over the number of timber assortment price classes (row count in puutavaralajimaarittelyt.txt)
-
-            # numpy array indexing: 1st row 2nd element: arr[0, 1]
-            # in R it's the same order: arr[2, 3] --> the item on 2nd row and 3rd column
-            # but whereas R-indexing is one-based, Python's is zero-based --> indexing has been offset by one
-            t = int(i + P[j,2] / div)
-            if t < n:
-                d_top = T[t,0]
-                d_min = P[j, 1]
-                
-                if d_top >= d_min:
-                    v = T[t,2] - T[i,2]
-                    c = v * P[j, 3]
-                    v_tot = v + V[i]
-                    c_tot = c + C[i]
-
-                    if c_tot > C[t]:
-                        V[t] = v_tot
-                        C[t] = c_tot
-                        A[t] = P[j, 0]
-                        L[t] = i
-                    
-
-    maxi = np.argmax(C)
-
-    nas = np.unique(P[:, 0])
-
-    volumes = np.zeros(len(nas))
-    values = np.zeros(len(nas))
-
-    a = l = 1
-
-    while maxi > 0:
-        a = int(A[maxi])-1 # a is used as an index for volumes and values. Here in Python, it must be subtracted by one to work with zero-based indexing.
-        l = int(L[maxi])
-        volumes[a] = volumes[a] + V[maxi] - V[l]
-        values[a] = values[a] + C[maxi] - C[l]
-
-        maxi = l
-
-    return (volumes, values) 
-
-
-    
-
