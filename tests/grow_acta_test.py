3a4b6c4c82a2bc51ae63ada7b924e97a314a11b3
Tapio Lempinen <ext.tapio.lempinen@luke.fi>
2022-12-15 11:43:50 +0200

    ref: return vectors from `grow_acta`

--- tests/grow_acta_test.py
+++ tests/grow_acta_test.py
@@ -4,9 +4,8 @@ from forestryfunctions.naturalprocess import grow_acta
 
 class GrowActaTest(unittest.TestCase):
     def test_yearly_diameter_growth_by_species(self):
-        tree = ReferenceTree()
-        tree.breast_height_diameter = 10.0
-        tree.height = 12.0
+        breast_height_diameter = 10.0
+        height = 12.0
         biological_age_aggregate = 35.0
         d13_aggregate = 34.0
         height_aggregate = 33.0
@@ -17,9 +16,10 @@ class GrowActaTest(unittest.TestCase):
             (2, 2.9096)
         ]
         for i in assertations_by_species:
-            tree.species = i[0]
             result = grow_acta.yearly_diameter_growth_by_species(
-                tree,
+                i[0],
+                breast_height_diameter,
+                height,
                 biological_age_aggregate,
                 d13_aggregate,
                 height_aggregate,
@@ -28,26 +28,24 @@ class GrowActaTest(unittest.TestCase):
             self.assertEqual(i[1], round(result, 4))
 
     def test_yearly_height_growth_by_species(self):
-        tree = ReferenceTree()
-        tree.breast_height_diameter = 10.0
-        tree.height = 12.0
+        breast_height_diameter = 10.0
+        height = 12.0
         biological_age_aggregate = 35.0
         d13_aggregate = 34.0
         height_aggregate = 33.0
-        dominant_height = 15.0
         basal_area_total = 32.0
         assertations_by_species = [
             (1, 3.9595),
             (2, 1.5397)
         ]
         for i in assertations_by_species:
-            tree.species = i[0]
             result = grow_acta.yearly_height_growth_by_species(
-                tree,
+                i[0],
+                breast_height_diameter,
+                height,
                 biological_age_aggregate,
                 d13_aggregate,
                 height_aggregate,
-                dominant_height,
                 basal_area_total)
             self.assertEqual(i[1], round(result, 4))
 
@@ -66,31 +64,25 @@ class GrowActaTest(unittest.TestCase):
                 biological_age=age)
             for d, h, f, spe, age in zip(diameters, heights, stems, species, ages)
         ]
-        result = grow_acta.grow_diameter_and_height(reference_trees)
-        self.assertEqual(21.5922, round(result[0].breast_height_diameter, 4))
-        self.assertEqual(24.2359, round(result[0].height, 4))
-        self.assertEqual(22.9209, round(result[1].breast_height_diameter, 4))
-        self.assertEqual(24.443, round(result[1].height, 4))
-        self.assertEqual(23.6697, round(result[2].breast_height_diameter, 4))
-        self.assertEqual(26.2199, round(result[2].height, 4))
+        resd, resh = grow_acta.grow_diameter_and_height(reference_trees)
+        self.assertEqual(21.5922, round(resd[0], 4))
+        self.assertEqual(24.2359, round(resh[0], 4))
+        self.assertEqual(22.9209, round(resd[1], 4))
+        self.assertEqual(24.443, round(resh[1], 4))
+        self.assertEqual(23.6697, round(resd[2], 4))
+        self.assertEqual(26.2199, round(resh[2], 4))
 
     def test_grow_sapling(self):
-        diameters = [1.0, 1.1, 1.2, None]
-        heights = [0.5, 0.9, 1.2, 1.6]
+        diameters = [1.0, 1.1, 1.2]
+        heights = [0.5, 0.9, 1.2]
         sapling_trees = [
-            ReferenceTree(breast_height_diameter=d, height=h, sapling=True)
+            ReferenceTree(breast_height_diameter=d, height=h)
             for d, h in zip(diameters, heights)
         ]
-        result = grow_acta.grow_saplings(sapling_trees)
-        self.assertEqual(1.0, result[0].breast_height_diameter)
-        self.assertEqual(1.1, result[1].breast_height_diameter)
-        self.assertEqual(1.2, result[2].breast_height_diameter)
-        self.assertEqual(1.0, result[3].breast_height_diameter)
-        self.assertEqual(0.8, result[0].height)
-        self.assertEqual(1.2, result[1].height)
-        self.assertEqual(1.5, result[2].height)
-        self.assertEqual(1.6, result[3].height)
-        self.assertEqual(True, result[0].sapling)
-        self.assertEqual(True, result[1].sapling)
-        self.assertEqual(False, result[2].sapling)
-        self.assertEqual(False, result[3].sapling)
+        resd, resh = grow_acta.grow_diameter_and_height(sapling_trees)
+        self.assertEqual(1.0, resd[0])
+        self.assertEqual(1.1, resd[1])
+        self.assertEqual(1.2, resd[2])
+        self.assertEqual(0.8, resh[0])
+        self.assertEqual(1.2, resh[1])
+        self.assertEqual(1.5, resh[2])
