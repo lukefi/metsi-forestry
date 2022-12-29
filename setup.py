0e0c69e2f2f651e85478c813d2ea56e3f7e3fb6f
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-12-29 17:15:18 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="forestryfunctions",
     description="Forestry functions for manipulating state of forest-data-model instances",
-    version="1.0.0-rc1",
+    version="1.0.0-rc2",
     packages=setuptools.find_namespace_packages(include=['forestryfunctions*']),
     package_data={'forestryfunctions': ['r/*']},
     dependency_links=['https://github.com/menu-hanke/forest-data-model@1.0.0-rc1#egg=forestdatamodel'],
