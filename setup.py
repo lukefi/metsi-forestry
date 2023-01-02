bbdaff37b3dc589e4a26bccbc372b0dfe67593ea
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2023-01-02 11:08:29 +0200

    chore: add missing package data export

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 setup(
     name="forestryfunctions",
     description="Forestry functions for manipulating state of forest-data-model instances",
-    version="1.0.0-rc2",
+    version="1.0.0-rc3",
     packages=setuptools.find_namespace_packages(include=['forestryfunctions*']),
-    package_data={'forestryfunctions': ['r/*']},
+    package_data={'forestryfunctions': ['r/*', 'lua/*']},
     dependency_links=['https://github.com/menu-hanke/forest-data-model@1.0.0-rc1#egg=forestdatamodel'],
     install_requires=[
         "scipy==1.7.*"
